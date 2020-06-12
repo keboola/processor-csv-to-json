4fb5d706517e1d1a5f0b93b80e1fc2969ccde945
David <david@keboola.com>
2020-06-12 14:41:39 +0200

    TODOs

--- src/csv2json/hone_csv2json.py
+++ src/csv2json/hone_csv2json.py
@@ -26,6 +26,7 @@ class Csv2JsonConverter(hone.Hone):
     def populate_structure_with_data(self, row, coltypes, delimit):
         # TODO: add recursion to enable "unlimited" levels
         # TODO: add support for JSON column type -> value is json parsable string like {} or []
+        # TODO: add datatype inference
         json_struct = []
         num_columns = len(self.column_names)
         processed_row = row
