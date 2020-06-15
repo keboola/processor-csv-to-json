6ba3d1e9c54d8bc39be3d341d69ad7806efca19f
David <david@keboola.com>
2020-06-15 15:46:16 +0200

    FEATURE datatype inferrence

--- src/csv2json/hone_csv2json.py
+++ src/csv2json/hone_csv2json.py
@@ -5,6 +5,7 @@ import logging
 import sys
 
 import hone
+import strconv
 
 
 class Csv2JsonConverter(hone.Hone):
@@ -18,15 +19,34 @@ class Csv2JsonConverter(hone.Hone):
         self.set_csv_filepath(csv_file_path)
         self.column_names = self.csv.get_column_names()
         self.column_struct = self.generate_full_structure(self.column_names)
+        self.str_converter = strconv.Strconv()
+        self._setup_converter()
 
-    def convert_row(self, row, coltypes, delimit):
+    def convert_object(self, s):
+        s = s.strip()
+        if s == "":
+            return None
+        try:
+            if (s.startswith('{') and s.endswith('}')) or (s.startswith('[') and s.endswith(']')):
+                return ast.literal_eval(s)
+            else:
+                raise ValueError(f'{s}Not object type.')
+        except Exception:
+            raise ValueError(f'{s}Not object type.')
+
+    def _setup_converter(self):
+        self.str_converter.register_converter('obj', self.convert_object)
+        self.str_converter.register_converter('int', strconv.convert_int)
+        self.str_converter.register_converter('float', strconv.convert_float)
+        self.str_converter.register_converter('bool', strconv.convert_bool)
+
+    def convert_row(self, row, coltypes, delimit, infer_undefined=False):
         data = row
-        json_struct = self.populate_structure_with_data(data, coltypes, delimit)
+        json_struct = self.populate_structure_with_data(data, coltypes, delimit, infer_undefined)
         return json_struct
 
-    def populate_structure_with_data(self, row, coltypes, delimit):
+    def populate_structure_with_data(self, row, coltypes, delimit, infer_undefined=False):
         # TODO: add recursion to enable "unlimited" levels
-        # TODO: add datatype inference
         json_struct = []
         num_columns = len(self.column_names)
         processed_row = row
@@ -36,82 +56,52 @@ class Csv2JsonConverter(hone.Hone):
             cell = processed_row[i].replace('\'', '\\\'')
             column_name = self.column_names[i]
             c_name_splitted = column_name.split(delimit)
-            for j in coltypes:
-                if (str(j["column"])) == column_name:
-                    if j["type"] == 'number':
-                        cell = float(cell.strip())
-                        if len(c_name_splitted) == 1:
-                            json_row[c_name_splitted[0]] = cell
-                        elif len(c_name_splitted) == 2:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]] = cell
-                        elif len(c_name_splitted) == 3:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]] = cell
-                        elif len(c_name_splitted) == 4:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]][
-                                c_name_splitted[3]] = cell
-                        else:
-                            logging.info("Too many nesting levels!")
-                            sys.exit(1)
+            cell = self._convert_datatype(cell, coltypes, column_name, True)
 
-                    elif j["type"] == 'string':
-
-                        if len(c_name_splitted) == 1:
-                            json_row[c_name_splitted[0]] = cell
-                        elif len(c_name_splitted) == 2:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]] = cell
-                        elif len(c_name_splitted) == 3:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]] = cell
-                        elif len(c_name_splitted) == 4:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]][
-                                c_name_splitted[3]] = cell
-                        else:
-                            logging.info("Too many nesting levels!")
-                            sys.exit(1)
-
-                    elif j["type"] == 'bool':
-
-                        if cell == 'True':
-                            cell = True
-                        elif cell == 'False':
-                            cell = False
-                        else:
-                            logging.info(
-                                "Value provided for boolean is not True or False. Please set the column\
-                                     in storage to type boolean!")
-                            sys.exit(1)
-                        if len(c_name_splitted) == 1:
-                            json_row[c_name_splitted[0]] = cell
-                        elif len(c_name_splitted) == 2:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]] = cell
-                        elif len(c_name_splitted) == 3:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]] = cell
-                        elif len(c_name_splitted) == 4:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]][
-                                c_name_splitted[3]] = cell
-                        else:
-                            logging.info("Too many nesting levels!")
-                            sys.exit(1)
-                    elif j["type"] == 'object':
-                        if cell:
-                            cell = ast.literal_eval(cell)
-                        if len(c_name_splitted) == 1:
-                            json_row[c_name_splitted[0]] = cell
-                        elif len(c_name_splitted) == 2:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]] = cell
-                        elif len(c_name_splitted) == 3:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]] = cell
-                        elif len(c_name_splitted) == 4:
-                            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]][
-                                c_name_splitted[3]] = cell
-                        else:
-                            logging.info("Too many nesting levels!")
-                            sys.exit(1)
-                    else:
-                        logging.info(
-                            'datatype for %s is not set, treating it as a string' % column_name)
+            self._fill_value_on_level(json_row, c_name_splitted, cell)
             i += 1
         json_struct.append(json_row)
         return json_struct
+
+    def _convert_datatype(self, cell, coltypes, column_name, infer_undefined=False):
+        cell = cell.strip()
+        colnames = [ct['column'] for ct in coltypes]
+        # if set to infer infer all undefined fields
+        if infer_undefined and column_name not in colnames:
+            cell = self.str_converter.convert(cell)
+        try:
+            for j in coltypes:
+                if (str(j["column"])) == column_name:
+                    if j["type"] == 'number':
+                        cell = strconv.convert_float(cell.strip())
+                    elif j["type"] == 'string':
+                        pass
+                    elif j["type"] == 'bool':
+                        cell = strconv.convert_bool(cell)
+                    elif j["type"] == 'object':
+                        if cell:
+                            cell = self.convert_object(cell)
+                    else:
+                        logging.info(
+                            'datatype for %s is not set, treating it as a string' % column_name)
+        except ValueError as e:
+            logging.exception(f'The value {cell} does not match the type: {j["type"]}')
+            sys.exit(1)
+        return cell
+
+    def _fill_value_on_level(self, json_row, c_name_splitted, cell):
+        if len(c_name_splitted) == 1:
+            json_row[c_name_splitted[0]] = cell
+        elif len(c_name_splitted) == 2:
+            json_row[c_name_splitted[0]][c_name_splitted[1]] = cell
+        elif len(c_name_splitted) == 3:
+            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]] = cell
+        elif len(c_name_splitted) == 4:
+            json_row[c_name_splitted[0]][c_name_splitted[1]][c_name_splitted[2]][
+                c_name_splitted[3]] = cell
+        else:
+            logging.info("Too many nesting levels!")
+            sys.exit(1)
 
     def get_schema(self, csv_filepath):
         self.set_csv_filepath(csv_filepath)
