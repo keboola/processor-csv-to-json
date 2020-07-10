446e10b244fb4a2674cb7fc0c3056b237dfce269
David <david@keboola.com>
2020-07-10 12:30:21 +0200

    UPDATE bump version setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='csv2json',
-    version='0.2.0',
+    version='0.3.3',
     setup_requires=['setuptools_scm'],
     url='https://bitbucket.org/kds_consulting_team/kds-team.processor-csv-to-json',
     download_url='https://bitbucket.org/kds_consulting_team/kds-team.processor-csv-to-json',
