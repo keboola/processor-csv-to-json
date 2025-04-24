6479f498fd00736ada14ca5fb37c7dc180282950
soustruh <martin@struzsky.cz>
2025-04-24 23:07:04 +0200

    0.5.8 🏷️

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import setuptools
 
 setup(
     name="csv2json",
-    version="0.5.7",
+    version="0.5.8",
     setup_requires=["setuptools_scm"],
-    url="https://bitbucket.org/kds_consulting_team/kds-team.processor-csv-to-json",
-    download_url="https://bitbucket.org/kds_consulting_team/kds-team.processor-csv-to-json",
+    url="https://github.com/keboola/processor-csv-to-json",
+    download_url="https://github.com/keboola/processor-csv-to-json",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
